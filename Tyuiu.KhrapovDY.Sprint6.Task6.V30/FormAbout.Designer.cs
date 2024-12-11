86561ffe7883981fdab751d5274efd237382d2f1
Daniil Khrapov <Tyuiu.KhrapovDY@outlook.com>
2024-12-11 09:36:19 +0500

    Task7

--- Tyuiu.KhrapovDY.Sprint6.Task6.V30/FormAbout.Designer.cs
+++ Tyuiu.KhrapovDY.Sprint6.Task6.V30/FormAbout.Designer.cs
@@ -29,64 +29,64 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAbout));
-            pictureBoxAvatar = new PictureBox();
-            labelInfo = new Label();
-            buttonOk = new Button();
-            ((System.ComponentModel.ISupportInitialize)pictureBoxAvatar).BeginInit();
+            pictureBoxAvatar_KDY = new PictureBox();
+            labelInfo_KDY = new Label();
+            buttonOk_KDY = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxAvatar_KDY).BeginInit();
             SuspendLayout();
             // 
-            // pictureBoxAvatar
+            // pictureBoxAvatar_KDY
             // 
-            pictureBoxAvatar.Image = (Image)resources.GetObject("pictureBoxAvatar.Image");
-            pictureBoxAvatar.Location = new Point(24, 31);
-            pictureBoxAvatar.Name = "pictureBoxAvatar";
-            pictureBoxAvatar.Size = new Size(254, 255);
-            pictureBoxAvatar.SizeMode = PictureBoxSizeMode.Zoom;
-            pictureBoxAvatar.TabIndex = 0;
-            pictureBoxAvatar.TabStop = false;
+            pictureBoxAvatar_KDY.Image = (Image)resources.GetObject("pictureBoxAvatar_KDY.Image");
+            pictureBoxAvatar_KDY.Location = new Point(24, 31);
+            pictureBoxAvatar_KDY.Name = "pictureBoxAvatar_KDY";
+            pictureBoxAvatar_KDY.Size = new Size(254, 255);
+            pictureBoxAvatar_KDY.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxAvatar_KDY.TabIndex = 0;
+            pictureBoxAvatar_KDY.TabStop = false;
             // 
-            // labelInfo
+            // labelInfo_KDY
             // 
-            labelInfo.AutoSize = true;
-            labelInfo.Location = new Point(349, 31);
-            labelInfo.Name = "labelInfo";
-            labelInfo.Size = new Size(609, 288);
-            labelInfo.TabIndex = 1;
-            labelInfo.Text = resources.GetString("labelInfo.Text");
+            labelInfo_KDY.AutoSize = true;
+            labelInfo_KDY.Location = new Point(349, 31);
+            labelInfo_KDY.Name = "labelInfo_KDY";
+            labelInfo_KDY.Size = new Size(609, 288);
+            labelInfo_KDY.TabIndex = 1;
+            labelInfo_KDY.Text = resources.GetString("labelInfo_KDY.Text");
             // 
-            // buttonOk
+            // buttonOk_KDY
             // 
-            buttonOk.Location = new Point(775, 348);
-            buttonOk.Name = "buttonOk";
-            buttonOk.Size = new Size(150, 46);
-            buttonOk.TabIndex = 2;
-            buttonOk.Text = "OK";
-            buttonOk.UseVisualStyleBackColor = true;
-            buttonOk.Click += buttonOk_Click;
+            buttonOk_KDY.Location = new Point(775, 348);
+            buttonOk_KDY.Name = "buttonOk_KDY";
+            buttonOk_KDY.Size = new Size(150, 46);
+            buttonOk_KDY.TabIndex = 2;
+            buttonOk_KDY.Text = "OK";
+            buttonOk_KDY.UseVisualStyleBackColor = true;
+            buttonOk_KDY.Click += buttonOk_Click;
             // 
             // FormAbout
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(980, 434);
-            Controls.Add(buttonOk);
-            Controls.Add(labelInfo);
-            Controls.Add(pictureBoxAvatar);
+            Controls.Add(buttonOk_KDY);
+            Controls.Add(labelInfo_KDY);
+            Controls.Add(pictureBoxAvatar_KDY);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "FormAbout";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "О программе";
-            ((System.ComponentModel.ISupportInitialize)pictureBoxAvatar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxAvatar_KDY).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private PictureBox pictureBoxAvatar;
-        private Label labelInfo;
-        private Button buttonOk;
+        private PictureBox pictureBoxAvatar_KDY;
+        private Label labelInfo_KDY;
+        private Button buttonOk_KDY;
     }
 }
